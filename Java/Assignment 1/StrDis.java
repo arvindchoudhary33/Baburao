faf4ee3fa19b3753dd2c6854f72d89f5e03bebcb
Bruce Wayne <67415371+iYashBorse@users.noreply.github.com>
2020-07-29 12:34:45 +0530

    Ass1 Set-A Q1
    This is the first program from assignment 1 set-A question 1 of Lab Book.

---
+++ Java/Assignment 1/StrDis.java
@@ -0,0 +1,7 @@
+public class StrDis{		//class declaration (same as file name)
+  public static void main(String ar[]){		//main function declaration
+    for(int i=0;i<ar.length;i++){		//accepting arguments from console
+        System.out.print(ar[i]+" ");		//printing arguments (a string as sequence of characters)
+    }
+  }
+}
