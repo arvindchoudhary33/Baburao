9bb99e0e59a254e43de93ed5bdc02b7e491dca78
amlp33 <68852345+amlp33@users.noreply.github.com>
2020-07-30 08:48:46 +0530

    addedFewComments

--- Java/Assignment 1/StrDis.java
+++ Java/Assignment 1/StrDis.java
@@ -1,3 +1,10 @@
+// Assign-1-Program-1
+// As it is the first program  ,  if you want to know why the file name is same as the public class name 
+// https://stackoverflow.com/questions/2134784/why-are-filenames-in-java-the-same-as-the-public-class-name  ( Don't be lazy , its really helpful ).
+
+// This program is in java to accept String from user and display it on console.
+
+
 public class StrDis{		//class declaration (same as file name)
   public static void main(String ar[]){		//main function declaration
     for(int i=0;i<ar.length;i++){		//accepting arguments from console
