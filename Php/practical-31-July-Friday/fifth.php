78d32ef47c2a319c3361ce652dd37d3a5391420c
amlp33 <sharmarahul965797@yahoo.com>
2020-07-31 21:53:40 +0530

    adding practical programs

---
+++ Php/practical-31-July-Friday/fifth.php
@@ -0,0 +1,66 @@
+
+<html>
+<head><title>characterChecker</title></head>
+<body bgcolor="beige">
+
+	<h1 align="center"> characterChecker-5th-Program </h1>
+
+<form method ="POST" >
+
+	EnterA_Character :<input type ="text" name="ch"><br><br>
+
+
+
+ <br><br><input type="submit">
+
+
+
+
+
+
+
+
+</form>
+
+
+
+
+
+<?php 
+  
+$char = $_POST["ch"];
+
+$value = ord($char);
+
+if($value >= 97 && $value <= 122 ){
+	echo"The Entered Character is verySmall";
+}
+else if($value >= 65 && $value <=90){
+
+	echo"The Entered Character is veryVeryBig";
+
+}
+
+else if($value >=48 && $value <= 57){
+
+	echo"Number hai!!";
+}
+
+else{
+
+	echo"It's a outOfWorldCharacter , I mean special.";
+}
+
+
+
+?> 
+
+</body>
+</html>
+
+	
+
+
+		
+
+		
