3455823a8fadc7b2376653dce6d642824acd9929
rutwik-shete <rutwik.shete@gmail.com>
2020-09-02 02:04:05 +0530

    Cricket Player Code Added And Edited

--- J/Assignment-0.2/set-B/CricPlay.java
+++ J/Assignment-0.2/set-B/CricPlay.java
@@ -1,41 +1,89 @@
 import java.util.Scanner;
-public class CricPlay{
-  int jerNo; String name; int runs;
-  int inn; int notOut; int avgRun;
-  //int[] allRuns; int[] allInn;
-  Scanner sc=new Scanner(System.in);
 
-  void GetData(){
+class Player{
+  private static int totalRuns=0 ;
+  private static int totalPlayers=0;
+  private int jerNo; 
+  private String name; 
+  private int runs;
+  private int inn; 
+  private int notOut; 
+
+  Scanner input = new Scanner(System.in);
+
+  public void GetData(){
     System.out.println("Enter Jersey number: ");
-    jerNo = sc.nextInt();
+    jerNo = input.nextInt();
     System.out.println("Enter Player's name: ");
-    name = sc.next();
+    name = input.next();
     System.out.println("Enter runs: ");
-    runs = sc.nextInt();
+    runs = input.nextInt();
     System.out.println("Enter innings played: ");
-    inn = sc.nextInt();
+    inn = input.nextInt();
     System.out.println("Enter number of times not out: ");
-    notOut = sc.nextInt();
+    notOut = input.nextInt();
+    totalRuns += runs;
+    totalPlayers++;
   }
 
-  void PutData(){
+  public void PutData(){
     System.out.println(jerNo+"\t"+name+"\t"+runs+"\t"+inn+"\t"+notOut+"\t Average runs: "+(runs/inn));
   }
 
-  public static void main(String[] ar){
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Enter number of players: ");
-    int n = sc.nextInt();
+  public String getName(){
+    return name;
+  }
 
-    CricPlay[] cp= new CricPlay[n];
-    for(int i=0;i<n;i++){
-      cp[i] = new CricPlay();
-      System.out.println("Enter details of player "+(i+1)+": ");
-      cp[i].GetData();
-    }
-    System.out.println("\n Details of Players is:\n");
-    for(int j=0; j<n; j++){
-      cp[j].PutData();
-    }
+  public int getRuns(){
+    return runs;
+  }
+
+  public int getInnings_played(){
+    return inn;
+  }
+
+  public static int getTotalPlayers(){
+      return totalPlayers;
+  }
+
+  public static int getTotalRuns(){
+    return totalRuns;
+  }
+}
+
+public class CricPlay{
+
+  public static void main(String[] ar){
+    Scanner input = new Scanner(System.in);
+    Player[] players= new Player[11];
+    int choice , playerNo;
+
+    do{
+      System.out.println("Enter: \n 0.To Exit \n 1.To Add Player \n 2.To Find Average Runs Of Single Player \n 3.To Find Average Runs Of All The Players");
+      choice = input.nextInt();
+      switch(choice){
+        case 0 : System.exit(0);
+        case 1 : players[Player.getTotalPlayers()] = new Player();
+                System.out.println("Enter details of player : ");
+                players[Player.getTotalPlayers()].GetData();
+                break;
+        case 2 : System.out.println("Enter The Player Whose Average We Want : ");
+                playerNo = input.nextInt();
+                getAverageRuns(players[playerNo]);
+                break;
+        case 3 : getAverageRuns();
+                break;
+        default : System.out.println("Wrong Choice !!!!!!!");
+      }
+    }while(choice != 0);
+    input.close();
+  }
+
+  public static void getAverageRuns(Player player){
+    System.out.println("Average Runs By "+player.getName()+" are : "+(player.getRuns()/player.getInnings_played()));
+  }
+
+  public static void getAverageRuns(){
+    System.out.println("Average Runs By The Team Are : " + (Player.getTotalRuns() / Player.getTotalPlayers()));
   }
 }
