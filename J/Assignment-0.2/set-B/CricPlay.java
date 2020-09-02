31e2685a615109d3b29cd6cc5792682bc573a935
rutwik-shete <rutwik.shete@gmail.com>
2020-09-02 15:32:39 +0530

    CricPlay.java edited

--- J/Assignment-0.2/set-B/CricPlay.java
+++ J/Assignment-0.2/set-B/CricPlay.java
@@ -31,6 +31,7 @@ class Player{
 
   public void PutData(){
     System.out.println(" Jersy Number : "+jerNo+"\n Player Name : "+name+"\n Player Runs : "+runs+"\n Player Innings : "+inn+"\n Player Not Out For : "+notOut+" matches");
+    System.out.println("------------------------------------------------------------------");
   }
 
   public String getName(){
@@ -67,22 +68,37 @@ public class CricPlay{
   public static void main(String[] ar){
     Scanner input = new Scanner(System.in);
     Player[] players= new Player[11];
+    Player player;
     int choice , playerNo;
 
     do{
-      System.out.println("Enter: \n 0.To Exit \n 1.To Add Player \n 2.To Find Average Runs Of Single Player \n 3.To Find Average Runs Of All The Players");
+      System.out.println("Enter: \n 0.To Exit \n 1.To Add Player \n 2.To View Single Player Details \n 3.To View All Team Details");
+      System.out.println(" 4.To Find Average Runs Of Single Player \n 5.To Find Average Runs Of All The Players");
       choice = input.nextInt();
       switch(choice){
+        // display all
+        //specific player
+        //sagle player
         case 0 : System.exit(0);
         case 1 : players[Player.getTotalPlayers()] = new Player();
                 System.out.println("Enter details of player : ");
                 players[Player.getTotalPlayers()].GetData();
                 break;
-        case 2 : System.out.println("Enter The Player Jersy Number Whose Average We Want : ");
+        case 2 : System.out.println("Enter The Player Jersy Number Whose Details You Want : ");
+                playerNo = input.nextInt();
+                player = findPlayer(playerNo, players);
+                player.PutData();
+                break;
+        case 3 :  for(int i = 0 ; i < Player.getTotalPlayers() ; i++){
+                    System.out.println("Details Player "+(i+1)+" : ");
+                    players[i].PutData();
+                  }
+                break;
+        case 4 : System.out.println("Enter The Player Jersy Number Whose Average You Want : ");
                 playerNo = input.nextInt();
                 getAverageRuns(playerNo,players);
                 break;
-        case 3 : getAverageRuns();
+        case 5 : getAverageRuns();
                 break;
         default : System.out.println("Wrong Choice !!!!!!!");
       }
@@ -90,18 +106,21 @@ public class CricPlay{
     input.close();
   }
 
+  public static Player findPlayer(int jersyNumber , Player[] players){
+      for(int i = 0 ; i < Player.getTotalPlayers() ; i++){
+        if(players[i].getJerNo() == jersyNumber){
+          return players[i];
+        }
+      }
+      System.out.println("The Player With The Jersy Number "+jersyNumber+" Not Found");
+      return null;
+  }
+
   public static void getAverageRuns(int playerNo , Player[] players){
-    Player playerFound = null;
-    for(int i = 0 ; i < Player.getTotalPlayers() ; i++){
-      if(players[i].getJerNo() == playerNo)
-        playerFound = players[i];
-    }
-    if(playerFound == null){
-      System.out.println("The Player With The Jersy Number "+playerNo+" Not Found");
-    }
-    else{
-      playerFound.PutData();
-      System.out.println("Average Runs By "+playerFound.getName()+" are : "+(playerFound.getRuns()/playerFound.getInnings_played()));
+    Player player = findPlayer(playerNo, players);
+    if(player != null){
+      player.PutData();
+      System.out.println("Average Runs By "+player.getName()+" are : "+(player.getRuns()/player.getInnings_played()));
     }
   }
 
